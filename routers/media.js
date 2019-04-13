7f155acb9eddbe52668153f709ee9dec8004911b
ruiqi <ruiqi@appledeMacBook-Air-2.local>
2019-04-13 17:34:49 -0400

    get result from cassandra

--- routers/media.js
+++ routers/media.js
@@ -14,14 +14,13 @@ var uniqid = require("uniqid");
 router.get('/:id', up.none(),function(req,res){
     console.log("Get a media file by its id.");
     console.log(req.params.id);
-    console.log(req.query.id);
     var query = 'SELECT content FROM medias WHERE id=?';
     var client = req.app.locals.client;
     client.execute(query, [req.params.id], function(err, result){
         if(err)
             res.json({'status':'error', 'error':err});
         else{
-            console.log('media is '+result[0])
+            console.log('media is '+result)
             res.json({'status':"OK"});
         }
     });
