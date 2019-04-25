59c2c79ccb46e5100a3741d48b0d15087e52d86f
ruiqi <ruiqi@xiamu.local>
2019-04-25 01:54:48 -0400

    ,

--- routers/addmedia.js
+++ routers/addmedia.js
@@ -16,18 +16,18 @@ router.post('/',upload.single('content'),function(req,res){
     console.log('add media check login');
     //console.log('cookies:'+req.cookies);
     if(req.cookies != undefined) {
-        console.log('cookies:'+req.cookies)
+        console.log('cookies: ',req.cookies);
         if(req.cookies.session != undefined){
-            console.log('session:'+req.cookies.session)
+            console.log('session:',req.cookies.session);
             if(req.cookies.session.current_user != null){
                 console.log('start to add media');
                 console.log(req.file);
                 var id = uniqid();
-                console.log("id is "+id);
+                console.log("id is ",id);
                 //console.log(req.file.buffer);
                 var client = req.app.locals.client;
                 var query = 'INSERT INTO medias (id, content, type) VALUES (?, ?, ?)';
-                console.log("upload file is "+req.file);
+                console.log("upload file is ",req.file);
                 if(req.file != undefined){
                     console.log(req.file.originalname.split('.')[1]);
                     client.execute(query, [id, req.file.buffer, req.file.originalname.split('.')[1]], function(err, result){
