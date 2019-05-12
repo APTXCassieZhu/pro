8497f68f7e8518d92f8950e11c6a042d24fd2f09
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-05-12 15:49:46 -0400

    Update add.js

--- routers/add.js
+++ routers/add.js
@@ -8,17 +8,27 @@ var uniqid = require("uniqid");
 
 router.post('/',function(req,res){
     console.log('start to add media');
-    var form = new formidable.IncomingForm();
-    form.parse(req);
-    console.log(form.parse(req));
+    
     var id = uniqid();
     console.log("id is "+id);
     //console.log(req.file.buffer);
-    var client = req.app.locals.client;
-    var query = 'INSERT INTO medias (id, content, type) VALUES (?, ?, ?)';
-    form.on('file', function (name, file){
-        console.log('Uploaded ' + file.name);
-    });
+    //var client = req.app.locals.client;
+    //var query = 'INSERT INTO medias (id, content, type) VALUES (?, ?, ?)';
+    var form = new formidable.IncomingForm().parse(req)
+        .on('file', function (name, file){
+            console.log('Uploaded ' + file.name);
+        })
+        .on('field', function(name, field) {
+            console.log('Got a field:', name);
+        })
+        .on('error', function(err) {
+            console.log(err);
+            next(err);
+        })
+        .on('end', function() {
+            console.log("end");
+            res.end();
+        });
     /*client.execute(query, [id, req.file.buffer, req.file.originalname.split('.')[1]], function(err, result){
         if(err)
             res.json({'status':'error', 'error':err});
